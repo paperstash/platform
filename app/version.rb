8d76e4f9392491a04c9a509562d24cc49d366a82
Michael Williams <m.t.williams@live.com>
2015-11-14 22:28:48 -0800

    Use the `PAPERSTASH_VERSION` enviornment variable, if specified.

--- app/version.rb
+++ app/version.rb
@@ -1,6 +1,6 @@
 module PaperStash
   def self.version
     # We don't have access to the Git repository (in Docker).
-    @version ||= File.read("#{PaperStash.root}/VERSION")
+    @version ||= ENV['PAPERSTASH_VERSION'] | File.read("#{PaperStash.root}/VERSION")
   end
 end
