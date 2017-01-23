eecde7754c0222ff1938dd2c3866e40f6ed40abe
Michael Williams <m.t.williams@live.com>
2017-01-22 18:24:07 -0800

    Add `PaperStash.Repository.paginate!` helper.
    Wraps the non-bang variant and raises an `ArgumentError` on failure.

--- apps/paperstash/lib/paperstash/repository.ex
+++ apps/paperstash/lib/paperstash/repository.ex
@@ -8,4 +8,13 @@ defmodule PaperStash.Repository do
 
   def paginate(pageable, request, options \\ []),
     do: Paginator.paginate(pageable, request, options)
+
+  def paginate!(pageable, request, options \\ []) do
+    case paginate(pageable, request, options) do
+      {:ok, page} ->
+        page
+      {:error, details} ->
+        raise ArgumentError, details
+    end
+  end
 end
