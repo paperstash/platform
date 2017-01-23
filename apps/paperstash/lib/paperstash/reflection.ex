d22b1b4759155a74b2c0420adde52f5c746cd5ca
Michael Williams <m.t.williams@live.com>
2017-01-22 18:25:44 -0800

    Add mapping of models to appropriate serializers.

--- apps/paperstash/lib/paperstash/reflection.ex
+++ apps/paperstash/lib/paperstash/reflection.ex
@@ -5,12 +5,23 @@ defmodule PaperStash.Reflection do
   @types %{
     person: PaperStash.Person,
     user: PaperStash.User,
-    token: PaperStash.Token
+    token: PaperStash.Token,
+    follow: PaperStash.Follow
   }
 
   for {type, mod} <- @types do
     def type(unquote(mod)), do: unquote(type)
     def type(%unquote(mod){}), do: unquote(type)
     def mod(unquote(type)), do: unquote(mod)
+  end
+
+  @serializers %{
+    PaperStash.User => PaperStash.UserSerializer,
+    PaperStash.Follow => PaperStash.FollowSerializer
+  }
+
+  for {mod, serializer} <- @serializers do
+    def serializer(unquote(mod)), do: unquote(serializer)
+    def serializer(%unquote(mod){}), do: unquote(serializer)
   end
 end
