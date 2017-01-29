d0e636e8dbf433844bce285d7599f598d0d5f670
Michael Williams <m.t.williams@live.com>
2017-01-28 16:21:27 -0800

    Extend response helpers to allow user-specified status codes.

--- apps/paperstash_web/lib/paperstash/web/helpers/response.ex
+++ apps/paperstash_web/lib/paperstash/web/helpers/response.ex
@@ -2,17 +2,23 @@ defmodule PaperStash.Web.Response do
   @moduledoc ~S"""
   """
 
-  def plaintext(conn, response) do
+  def plaintext(conn, response),
+    do: plaintext(conn, 200, response)
+
+  def plaintext(conn, status, response) do
     conn |> Plug.Conn.put_resp_content_type("text/plain")
-         |> Plug.Conn.resp(200, response)
+         |> Plug.Conn.resp(status, response)
          |> Plug.Conn.halt
   end
 
   @json_encoder_options pretty: Application.get_env(:knowledge_web, :pretty, true)
 
-  def json(conn, response) do
+  def json(conn, response),
+    do: json(conn, 200, response)
+
+  def json(conn, status, response) do
     conn |> Plug.Conn.put_resp_content_type("application/json")
-         |> Plug.Conn.send_resp(200, Poison.encode!(response, @json_encoder_options))
+         |> Plug.Conn.send_resp(status, Poison.encode!(response, @json_encoder_options))
          |> Plug.Conn.halt
   end
 end
--- apps/paperstash_web/lib/paperstash/web/routes.ex
+++ apps/paperstash_web/lib/paperstash/web/routes.ex
@@ -8,8 +8,8 @@ defmodule PaperStash.Web.Routes do
 
       use Plug.Router
 
-      import PaperStash.Web.Routes, only: [plaintext: 1,
-                                           json: 1,
+      import PaperStash.Web.Routes, only: [plaintext: 1, plaintext: 2,
+                                           json: 1, json: 2,
                                            paginate: 1, paginate: 2]
 
       alias PaperStash.Web.{NotImplementedError,
@@ -31,9 +31,21 @@ defmodule PaperStash.Web.Routes do
 
   # TODO(mtwilliams): Detect `__struct__` and serialize prior to encoding.
 
+  defmacro plaintext(status, response) do
+    quote do
+      PaperStash.Web.Response.plaintext(var!(conn), unquote(status), unquote(response))
+    end
+  end
+
   defmacro plaintext(response) do
     quote do
       PaperStash.Web.Response.plaintext(var!(conn), unquote(response))
+    end
+  end
+
+  defmacro json(status, response) do
+    quote do
+      PaperStash.Web.Response.json(var!(conn), unquote(status), unquote(response))
     end
   end
 
